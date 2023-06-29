2ba1d8bf9bb4e1cc5f03d79a63654d1061c0fc35
Alena Havaleshko <alena.havaleshko@gmail.com>
2023-06-29 16:33:20 +0300

    delete home page

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -12,7 +12,6 @@ import AuthLayout from '../layouts/AuthLayout';
 import MainLayout from '../layouts/MainLayout';
 
 //page
-import HomePage from './HomePage';
 import AccountPage from './AccountPage';
 import CalendarPage from './CalendarPage';
 
@@ -21,13 +20,9 @@ function Router () {
  return(
 <Routes>
   <Route path="/" element={<AuthRoute element={LogIn}/>}/>
-
   <Route path="/login" element= {<AuthRoute element={LogIn}/>}/>
   <Route path="/signup" element= {<AuthRoute element={SignUp}/>}/>
-
-  <Route path="/account" element= {<PublicRoute element={AccountPage}/>}/>  
-  <Route path="/home" element= {<PublicRoute element={HomePage}/>}/>  
-
+  <Route path="/account" element= {<PublicRoute element={AccountPage}/>}/> 
   <Route path='/calendar' element={<PublicRoute element={CalendarPage}/>}/>
 </Routes>
  );
